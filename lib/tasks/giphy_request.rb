3c67ee90dd382b71f635e76bc1490c3067559193
Tanya Newell <trozova5@gmail.com>
2016-07-17 12:27:24 -0400

    ruby on rails refresher

---
+++ lib/tasks/giphy_request.rb
@@ -0,0 +1,5 @@
+# require 'net/http'
+# require 'json'
+#
+# uri = URI('http://api.giphy.com/v1/gifs/search?q=funny+cat&api_key=dc6zaTOxFJmzC')
+# puts JSON.pretty_generate(JSON.parse(Net::HTTP.get(uri)))
